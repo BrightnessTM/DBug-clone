cffad6e3ae4bcfeeaf2f402f2f370a67f30b45ec
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-10-11 22:42:40 +0200

    :+1: Able to dapprove bugs

---
+++ src/commands/admin/dapprove.js
@@ -0,0 +1,43 @@
+const Report = require("../../models/report")
+const ReportHandler = require("../../handlers/report")
+const fs = require('fs');
+
+const json = fs.readFileSync("./src/config.json");
+const config = JSON.parse(json)
+
+const clc = require("cli-color");
+
+module.exports = {
+	name: "dapprove",
+	category: "admin",
+	description: "Admin approves a bug",
+	roles: ["763860080772775976", "763860111176237057"],
+	run: async (client, message, args) => {
+        const id = args[0]
+        const aMessage = args.slice(1).join(" ");
+
+        const r = async function (params) {
+			try {
+				return await Report.findOne(params);
+			} catch (err) {
+				console.error(clc.red(err));
+			}
+        };
+
+        message.delete()
+      
+        const foundReport = await r({reportID: id})
+        if(foundReport === null) return message.reply("Hmm that report doesnt exist :(").then(msg => msg.delete({ timeout: 3000 }))
+        else if (foundReport.stance === "Approved" ) return message.reply("This bug has already been approved.").then(msg => msg.delete({ timeout: 3000 }))
+        else if (foundReport.stance === "Denied") return message.reply("This bug has been denied.").then(msg => msg.delete({ timeout: 3000 }))
+
+
+        await Report.findOneAndUpdate(
+          { _id: foundReport._id }, 
+          { $push: { approves: `${config.emotes.green} ${message.author.username}#${message.author.discriminator}: ${aMessage}` },  stance: `Approved` },
+      );
+
+      ReportHandler.UpdateStance(client,  await r({reportID: id}))
+     
+  },
+};
